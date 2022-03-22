658cd30da3ef661f639c2a822c4cfdb9d7873103
ehsansam90 <65367646+ehsansam90@users.noreply.github.com>
2022-03-21 23:50:03 -0400

    calculateFitness

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-cities = [(1,2),(20,4),(5,6),(8,10)]
+cities = [(1,2),(1,3),(1,4)] # cities should be more than two
 order = [i for i in range(len(cities))]
 population = []
 
@@ -26,7 +26,7 @@ def calculate_dist(cities, population):
         y1 = cities[population[i]][1]
         y2 = cities[population[i + 1]][1]
 
-        dist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        dist = math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
         dist +=dist
     return(dist)
 
@@ -35,17 +35,23 @@ new_pop = create_population(order,100)
 # print(len(new_pop))
 # print(calculate_dist(cities, new_pop[0]))
 
-def fitness_func(population):
-    test = [(population[i], calculate_dist(cities, population[i])) for i in range(len(population))]
-        # [{str(population[i]):calculate_dist(cities,population[i])} for i in range(len(population)]
-    print(test)
+def calculateFitness(population):
+    # test = [(population[i], calculate_dist(cities, population[i])) for i in range(len(population))]
+    recorded_distence = calculate_dist(cities, population[0])
+    for pop in population:
+        d = calculate_dist(cities , pop)
+        print(d,pop)
+        if d <= recorded_distence:
+            recorded_distence = d
+            bestpop = pop
+    print(bestpop, recorded_distence)
 
 
 
 
 
 
-fitness_func(new_pop)
+calculateFitness(new_pop)
 
 
 
