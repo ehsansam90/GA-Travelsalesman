828d79b3a7293d92a50c45adfc3b687c2333dafb
ehsansam90 <65367646+ehsansam90@users.noreply.github.com>
2022-04-10 20:15:09 -0400

    edit

--- run.py
+++ run.py
@@ -1,11 +1,24 @@
 from Classes import gaPlot, City, ga
 import random
+import matplotlib.pyplot as plt
 
 cityList = []
 
-for i in range(0,20):
-    cityList.append(City(x=int(random.random() * 200), y=int(random.random() * 200)))
+for i in range(0,50):
+    cityList.append(City(x=int(random.random() * 1000), y=int(random.random() * 1000)))
 
-gaPlot(population=cityList, pop_size=30, elite_size=5, mutation_rate=0.01, generations=1000)
+gaPlot(population=cityList, pop_size=100, elite_size=20, mutation_rate=0.01, generations=1000)
 
 # ga(population=cityList, pop_size=10, elite_size=5, mutation_rate=0.01, generations=10)
+
+# for number of cities on 15,20,25,30,35,40,45,50
+# values = []
+# pop_size = []
+# for i in range(40,400 ,5):
+#     values.append(gaPlot(population=cityList, pop_size=i, elite_size=20, mutation_rate=0.01, generations=50))
+#     pop_size.append(i)
+#
+# print(values,pop_size)
+# plt.plot(values, pop_size)
+# plt.show()
+
