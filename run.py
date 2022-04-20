ae9ceb78ade5ebbe7c39eb67ea656b9b52b134f7
ehsansam90 <65367646+ehsansam90@users.noreply.github.com>
2022-04-20 18:51:26 -0400

    final edit

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ cityList = []
 for i in range(0,50):
     cityList.append(City(x=int(random.random() * 1000), y=int(random.random() * 1000)))
 
-gaPlot(population=cityList, pop_size=100, elite_size=20, mutation_rate=0.01, generations=1000)
+gaPlot(population=cityList, pop_size=100, elite_size=20, mutation_rate=0.01, generations=100)
 
-# ga(population=cityList, pop_size=10, elite_size=5, mutation_rate=0.01, generations=10)
+# ga(population=cityList, pop_size=10, elite_size=5, mutation_rate=0.01, generations=100)
 
 # for number of cities on 15,20,25,30,35,40,45,50
 # values = []
